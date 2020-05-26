f4adb7e4be96d5230192d34488be4ebd22f6005c
594521354@qq.com <XIAOjq5561812>
2020-05-26 02:32:57 -0700

    remove log

--- customers/src/main/java/com/coupon/customers/vo/CouponTemplateInfo.java
+++ customers/src/main/java/com/coupon/customers/vo/CouponTemplateInfo.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class CouponTemplateInfo extends CouponTemplate{
+public class CouponTemplateInfo {
     /** Coupon Template*/
     private CouponTemplate couponTemplate;
 
